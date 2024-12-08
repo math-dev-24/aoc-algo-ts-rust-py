5b71243c16ca6d945cc0d5f2bfe0df6c60367988
Mathieu <mathieu.busse@live.fr>
2024-12-08 15:17:28 +0100

    refactor day8 py

--- srcp/day-8.py
+++ srcp/day-8.py
@@ -7,33 +7,30 @@ lines = test.strip().replace("#", ".").splitlines()
 
 grid = [list(map(str, line)) for line in lines]
 
-list = {}
 
-for row in range(len(lines)):
-    chars = [(col, char) for (col, char) in enumerate(lines[row]) if char != '.']
-    if chars:
-        for col, char in chars:
-            if char not in list:
-                list[char] = []
-            list[char].append((row, col))
+def get_antennas(q_row: int) -> dict:
+    tmp_list = {}
+    for row in range(q_row):
+        chars = [(col, char) for (col, char) in enumerate(lines[row]) if char != '.']
+        if chars:
+            for col, char in chars:
+                if char not in tmp_list:
+                    tmp_list[char] = []
+                tmp_list[char].append((row, col))
+    return tmp_list
 
 
+list_antennas: dict = get_antennas(len(grid[0]))
 anti_nodes = set()
 
-#print(list)
+for char, antennas in list_antennas.items():
+    if len(antennas) > 1:
+        anti_nodes.update(antennas)
 
-for frequency, antennes in list.items():
-    # Pour la partie 2 compter les entennes
-    if len(antennes) > 1:
-        anti_nodes.update(antennes)
-
-    for a1, a2 in combinations(antennes, 2):
+    for a1, a2 in combinations(antennas, 2):
         y1, x1 = a1
         y2, x2 = a2
-        # DX et DY sont la distance entre les deux points
         dx, dy = x2 - x1, y2 - y1
-
-        #print(f"dx: {dx}, dy: {dy}")
 
         factor = 1
         while True:
@@ -46,16 +43,13 @@ for frequency, antennes in list.items():
             n1 = (y1 - scaled_dy, x1 - scaled_dx)
             n2 = (y2 + scaled_dy, x2 + scaled_dx)
 
-            # print(f"n1: {n1}, n2: {n2}")
-
             if not is_in_grid(n1) and not is_in_grid(n2):
                 break
-            if n1 not in antennes and is_in_grid(n1):
+
+            if is_in_grid(n1):
                 anti_nodes.add(n1)
-            if n2 not in antennes and is_in_grid(n2):
+            if is_in_grid(n2):
                 anti_nodes.add(n2)
             factor += 1
 
 print(len(anti_nodes))
-
-
