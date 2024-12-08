2a7cde8d5d047a6f4b73098a05a7af2c1cff9ecb
Mathieu <mathieu.busse@live.fr>
2024-12-08 14:59:21 +0100

    update function for iter

--- src/utils/iterTools.ts
+++ src/utils/iterTools.ts
@@ -1,25 +1,26 @@
-
 export function combinations<T>(array: T[], r: number): T[][] {
     const result: T[][] = [];
     const n = array.length;
 
-    if (r > n) return result;
+    if (r > n || r <= 0) return result;
 
     const indices = Array.from({ length: r }, (_, i) => i);
 
-    while (true) {
+    do {
         result.push(indices.map((i) => array[i]));
 
         let i = r - 1;
-        while (i >= 0 && indices[i] === i + n - r) {
-            i--;
-        }
-        if (i < 0) break;
+        while (i >= 0 && indices[i] >= n - r + i) i--;
 
-        indices[i]++;
-        for (let j = i + 1; j < r; j++) {
-            indices[j] = indices[j - 1] + 1;
+        if (i >= 0) {
+            indices[i]++;
+            for (let j = i + 1; j < r; j++) {
+                indices[j] = indices[j - 1] + 1;
+            }
+        } else {
+            break;
         }
-    }
+    } while (true);
+
     return result;
 }
