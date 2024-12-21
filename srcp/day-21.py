b6544072284f2347e22ba2f27b183bd954c1cdc1
Mathieu <mathieu.busse@live.fr>
2024-12-21 13:18:15 +0100

    day21 PY part 1 reste Calcul

---
+++ srcp/day-21.py
@@ -0,0 +1,113 @@
+from typing import Optional
+
+numeric_pad = [
+    ["7", "8", "9"],
+    ["4", "5", "6"],
+    ["1", "2", "3"],
+    [None, "0", "A"]
+]
+
+arrow_pad = [
+    [None, "^", "A"],
+    ["<", "v", ">"]
+]
+
+int_pad = [
+    [None, "A", None],
+    ["^", None, ">"],
+    [None, "v", None],
+    ["<", None, None]
+]
+
+
+def find_position(target: str, keyboard: list[list[str]]) -> Optional[dict]:
+    for ri, row in enumerate(keyboard):
+        for ci, val in enumerate(row):
+            if val == target:
+                return {
+                    "position": (ri, ci),
+                    "value": val
+                }
+    return None
+
+
+def move_on_pad(start: tuple[int, int], end: tuple[int, int]) -> list[str]:
+    moves = []
+    si, sj = start
+    ei, ej = end
+    while si < ei:
+        moves.append("v")
+        si += 1
+    while si > ei:
+        moves.append("^")
+        si -= 1
+    while sj < ej:
+        moves.append(">")
+        sj += 1
+    while sj > ej:
+        moves.append("<")
+        sj -= 1
+    return moves
+
+
+def generate_sequence_for_code(code: str, start_position="A"):
+    current_position = find_position(start_position, numeric_pad)
+    sequence = []
+    for char in code:
+        target_position = find_position(char, numeric_pad)
+        moves = move_on_pad(current_position['position'], target_position['position'])
+        sequence.extend(moves)
+        sequence.append("A")
+        current_position = target_position
+    return sequence
+
+
+def get_arrow_dir_int(initial_position: tuple[int, int], target: str, pad: list[list[str]]) -> list[str]:
+    x, y = initial_position
+    x_target, y_target = find_position(target, pad)["position"]
+    result = []
+
+    while x != x_target or y != y_target:
+        if x_target < x:
+            result.append("^")
+            x -= 1
+        if x_target > x:
+            result.append("v")
+            x += 1
+        if y_target < y:
+            result.append("<")
+            y -= 1
+        if y_target > y:
+            result.append(">")
+            y += 1
+    result.append("A")
+    return result
+
+
+def transform_command(list_sequence: list[str], pad: list[list[str]], start_position="A") -> list[str]:
+    current_position = find_position(start_position, pad)
+
+    x, y = current_position["position"]
+    sequences = []
+
+    for i, command in enumerate(list_sequence):
+        # print(f"Commande : {i + 1}: {command}")
+        new_command = get_arrow_dir_int((x, y), command, arrow_pad)
+        # print(new_command)
+        sequences.extend(new_command)
+        x, y = find_position(command, arrow_pad)["position"]
+
+    return sequences
+
+
+
+code = "029A"
+numeric_sequence = generate_sequence_for_code(code)
+print("Séquence pavé numérique :", "".join(numeric_sequence))
+
+intermediate_sequence = transform_command(numeric_sequence, arrow_pad)
+print("Séquence pavé intermédiaire :", "".join(intermediate_sequence))
+
+human_sequence = transform_command(intermediate_sequence, int_pad)
+print("Séquence pavé humaine :", "".join(human_sequence))
+
